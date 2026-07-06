88d6570dabaf2ab5140bec266f5792a54059d442
geraltlambkebab <geraltlambkebab@gmail.com>
2026-07-06 21:49:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './Plant-for-agger/icon-192.png',
-  './Plant-for-agger/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
